aba855e6ed101b7466aed3c09842e980923113bc
OkamiGJ <jhoseph.villegas@gmail.com>
2026-07-23 21:11:29 -0400

    Se creo la base de datos y se agregaron la visualizacion de datos en la pokedex

--- jhoseph/src/App.js
+++ jhoseph/src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { pokeData } from "./componentes/pokelist";
 
 // Puedes dejar el componente PokedexHud aquí mismo
 function PokedexHud({ childrenLeft, childrenRight }) {
+
+    const [disponibles, setDisponibles] = useState(pokeData.map((p)=>p.id)); /*paraque no se repitan pokemons*//*miedo en el rancho*/
+
+    const [match, setMatch] = useState(()=>{
+        const randomIndex = Math.floor(Math.random() * pokeData.length);    
+        return pokeData[randomIndex];
+    })/*muestra el match actual*/
+
+    const nextMatch=()=>{
+        const restantes=disponibles.filter((id)=> id !== match.id);
+        if (restantes.length === 0) {
+            alert("¡Has visto a todos los Pokémon! Reiniciando la Pokédex...");
+            const nuevaLista = pokeData
+                .map((p) => p.id)
+                .filter((id) => id !== match.id);
+            setDisponibles(nuevaLista);
+
+            const randomId = nuevaLista[Math.floor(Math.random() * nuevaLista.length)];
+            setMatch(pokeData.find((p) => p.id === randomId));
+            return;
+    }
+
+    const randomId = restantes[Math.floor(Math.random() * restantes.length)];
+        const siguiente = pokeData.find((p) => p.id === randomId);
+
+        setMatch(siguiente);
+        setDisponibles(restantes);
+    };
+
     return (
         <div className="pokedex-hud">
 
@@ -17,11 +47,25 @@ function PokedexHud({ childrenLeft, childrenRight }) {
                     </div>
                 </div>
 
+
                 <div className="panel-central">
                     <div className="screen-content">
-                        {childrenLeft || <p>IMAGEN POKEMON</p>}
+                        { match?.imagen ? (
+                            <img
+                                src={match.imagen}
+                                alt={match.nombre}
+                                style={{
+                                    height: "100%",
+                                    width: "100%",
+                                    objectFit: "contain",
+                                }}
+                            />
+                        ) : (
+                            <p>No Imagen</p>
+                        )}
                     </div>
                 </div>
+
 
                 <div className="controls">
                     <div className="pill-buttons">
@@ -32,12 +76,25 @@ function PokedexHud({ childrenLeft, childrenRight }) {
 
                 <div className="panel">
                     <div className="control">
-                        <div className="pokemon-name">NombrePokemon</div>
-                        <div className="pokemon-descripcion">DescripcionPokemon</div>
+                        <div className="pokemon-name">{match?.nombre || "Cargando..."}
+                        </div>
+                        <div className="pokemon-descripcion">
+                            <div className="visualp1">
+                                Nivel:{match?.nivel} | Genero:{match?.genero}
+                            </div>
+                            <div className="visualp2">
+                                {match?.descripcion}
+                            </div>
+                        </div>
                     </div>
+
                   <div className="botones">
-                    <button className="boton smash">SELECT</button>
-                    <button className="boton pass">PASS</button>
+                    <button className="boton smash" onClick={nextMatch}>
+                            SMASH
+                        </button>
+                        <button className="boton pass" onClick={nextMatch}>
+                            PASS
+                        </button>
                   </div>
                 </div>
             </div>
@@ -49,7 +106,6 @@ function PokedexHud({ childrenLeft, childrenRight }) {
                 <div className="pantalla-derecha">
                   <button className="menu-btn">Historial</button>
                   <button className="menu-btn">Perfil</button>
-                  <button className="menu-btn">Ajustes</button>
                 </div>
               </div>
             </div>
